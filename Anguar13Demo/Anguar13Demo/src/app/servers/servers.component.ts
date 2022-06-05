4439c15f5d41c2e9662ec744fdf52589b8515bd8
Carlos <carlosjavierlopezlopez@gmail.com>
2022-06-05 11:28:44 +0200

    Data binding lecture

--- Anguar13Demo/Anguar13Demo/src/app/servers/servers.component.ts
+++ Anguar13Demo/Anguar13Demo/src/app/servers/servers.component.ts
@@ -6,5 +6,22 @@ import { Component} from "@angular/core";
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent{
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created';
+  serverName = 'Test server';
 
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    },
+    2000);
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server created ' + this.serverName;
+  }
+
+  onUpdateServerName(event: any) {    
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
